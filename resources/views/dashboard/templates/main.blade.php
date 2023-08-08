2c492e752df2617ed0c9d8caf6045e1a108d9fa9
Gustavo Valdez <gvaldezsan@gmail.com>
2023-08-08 06:52:05 -0600

    Standardize Dashboard to Line Awesome Icons

--- resources/views/dashboard/templates/main.blade.php
+++ resources/views/dashboard/templates/main.blade.php
@@ -10,6 +10,7 @@
         <meta name="description" content="Centro de operaciones para Vatmex. CTA's y Staff solamente">
         <meta name="author" content="Gustavo Valdez">
         <title>Dashboard | Vatmex Ops</title>
+        <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
 
         <!-- BEGIN: Vendor CSS-->
