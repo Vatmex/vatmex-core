027384483601acc69bff6462aaf89a196b621097
Gustavo Valdez <gvaldezsan@gmail.com>
2023-08-20 17:33:08 -0600

    Add HTMLPurifier for relevant models

--- app/Models/TrainingNote.php
+++ app/Models/TrainingNote.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Mews\Purifier\Casts\CleanHtml;
 
 class TrainingNote extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'message' => CleanHtml::class,
+    ];
 
     public function student()
     {
--- app/Models/TrainingSession.php
+++ app/Models/TrainingSession.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Mews\Purifier\Casts\CleanHtml;
 
 class TrainingSession extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'description' => CleanHtml::class,
+        'cancelation_motive' => CleanHtml::class,
+    ];
 
     public function student()
     {
